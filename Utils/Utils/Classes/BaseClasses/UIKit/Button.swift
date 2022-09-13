045cdbaa77e466ebdca2dd1b070a8fe17a8aa794
Maksim Sashcheka <maksim.sashcheka@yellow.systems>
2022-09-14 01:21:31 +0300

    Implement ViewController base class

---
+++ Utils/Utils/Classes/BaseClasses/UIKit/Button.swift
@@ -0,0 +1,8 @@
+//
+//  Button.swift
+//  Utils
+//
+//  Created by Maksim Sashcheka on 14.09.22.
+//
+
+import Foundation
---
+++ Utils/Utils/Classes/BaseClasses/UIKit/Label.swift
@@ -0,0 +1,8 @@
+//
+//  Label.swift
+//  Utils
+//
+//  Created by Maksim Sashcheka on 14.09.22.
+//
+
+import Foundation
